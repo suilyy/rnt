af81543d3dbd81f0a5775c4ab3faedea42690d9f
huangzongzheng <1518931313@qq.com>
2024-09-23 17:39:28 +0800

    feat:2024-phase2-tester用例集成（补充）

--- 2024-phase2-tester/harmony/entry/src/main/ets/RNPackagesFactory.ts
+++ 2024-phase2-tester/harmony/entry/src/main/ets/RNPackagesFactory.ts
@@ -1,6 +1,7 @@
 import type {RNPackageContext, RNPackage} from '@rnoh/react-native-openharmony/ts';
-
-
+import { DocumentPickerPackage } from '@react-native-oh-tpl/react-native-document-picker/ts';
 export function createRNPackages(ctx: RNPackageContext): RNPackage[] {
-  return [];
+  return [
+    new DocumentPickerPackage(ctx)
+  ];
 }
