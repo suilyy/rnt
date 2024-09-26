1ed2db1dada20a58fa0c13ac37434fb50f44a5c2
huangzz1106 <106304144+huangzz1106@users.noreply.github.com>
2024-09-26 14:41:09 +0800

    feat: 修改react-native-ezswiper自动化用例

--- 2024-phase2-tester/rn-tester/examples/react-native-ezswiper/testIndex.js
+++ 2024-phase2-tester/rn-tester/examples/react-native-ezswiper/testIndex.js
@@ -20,7 +20,7 @@ import EZSwiper from 'react-native-ezswiper';
 import { Tester, TestSuite } from '@rnoh/testerino';
 import { TestCase } from './TestCase';
 
-const images = [require(`./resource/0.jpg`), require(`./resource/1.jpg`), require(`./resource/2.jpg`), require(`./resource/3.jpg`), require(`./resource/4.jpg`), require(`./resource/5.png`), require(`./resource/6.jpg`)]
+const images = [require(`./resource/0.jpg`), require(`./resource/1.jpg`), require(`./resource/2.jpg`), require(`./resource/3.jpg`), require(`./resource/4.jpg`), require(`./resource/5.jpg`), require(`./resource/6.jpg`)]
 
 export default class ezswiperApp extends Component<{}> {
   constructor(props) {
