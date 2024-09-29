175cec811ff6d19a55c7baf8bbb95e77e905a4a3
xbh0902 <xbh0902@gmail.com>
2024-09-29 17:27:30 +0800

    feat: 更新viewshot测试用例

--- 2024-phase4-tester/rn-tester/examples/react-native-view-shot/Test/ViewShotContinuousTest.tsx
+++ 2024-phase4-tester/rn-tester/examples/react-native-view-shot/Test/ViewShotContinuousTest.tsx
@@ -1,29 +1,34 @@
 import React, { useState } from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text,  ScrollView } from "react-native";
 import ViewShot from "react-native-view-shot";
 import { Tester, TestCase } from '@rnoh/testerino';
 
-export default function ViewShotMountTest() {
-  const ViewShotMount = (props: {
+export default function ViewShotContinuousTest() {
+  const ViewShotContinuous = (props: {
     setState: React.Dispatch<React.SetStateAction<string>>;
   }) => {
     const [mountUri, setMountUri] = useState('');
-    const myMount = React.useRef<ViewShot>(null);
+    const [errMsg, setErrMsg] = useState('');
+    const myText = React.useRef<ViewShot>(null);
     const getSuccess = (res) => {
+      console.log('captureSuccess', res);
       setMountUri(res);
-      props.setState(res);
-    }
-    const getError = (err) => {
-      console.log(err.message);
     }
 
+    const getError = (err) => {
+      console.log('captureFailed', err.message);
+      setErrMsg(err.message);
+    }
     return (
-      <View style={{ height: 55 }}>
+      <View style={{ height: 80 }}>
         <View>
-          <ViewShot ref={myMount} style={{ backgroundColor: '#bfa' }} captureMode="mount" onCaptureFailure={getError} onCapture={getSuccess} children={<Text>测试截图</Text>}></ViewShot>
+          <ViewShot ref={myText} style={{ backgroundColor: '#bfa' }} captureMode="continuous" onCaptureFailure={getError} onCapture={getSuccess}>
+            <Text>测试截图</Text>
+          </ViewShot>
         </View>
         <View style={{ marginTop: 20 }}>
-        <Text numberOfLines={1} ellipsizeMode="middle">截图保存路径：{mountUri}</Text>
+          <Text numberOfLines={1} ellipsizeMode="middle">截图成功路径：{mountUri}</Text>
+          <Text numberOfLines={1} ellipsizeMode="middle">截图失败信息：{errMsg}</Text>
         </View>
       </View>
     );
@@ -34,9 +39,9 @@ export default function ViewShotMountTest() {
       <ScrollView>
         <Tester>
           <TestCase
-            itShould="captureMode为mount，onCapture接口返回截图成功的路径"
+            itShould="captureMode为continuous，onCapture属性返回截图成功的路径，onCaptureFailure属性返回截图失败信息"
             initialState={''}
-            arrange={({ setState }) => <ViewShotMount setState={setState} />}
+            arrange={({ setState }) => <ViewShotContinuous setState={setState} />}
             assert={({ expect, state }) => {
               expect(state).to.be.string;
             }}
