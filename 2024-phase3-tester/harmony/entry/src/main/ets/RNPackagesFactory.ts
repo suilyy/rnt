ed23c4003473926212ce0951ad6adf2888a244df
hewei <hewei203@h-partners.com>
2024-10-15 14:34:05 +0800

    test: 更新react-native-audio-recorder-player配置文件

--- 2024-phase3-tester/harmony/entry/src/main/ets/RNPackagesFactory.ts
+++ 2024-phase3-tester/harmony/entry/src/main/ets/RNPackagesFactory.ts
@@ -3,12 +3,14 @@ import { FastImagePackage } from '@react-native-oh-tpl/react-native-fast-image/t
 import { SvgPackage } from '@react-native-oh-tpl/react-native-svg/ts';
 import { SecureRandomPackage } from '@react-native-oh-tpl/react-native-securerandom/ts';
 import { WebViewPackage } from '@react-native-oh-tpl/react-native-webview/ts';
+import {RNAudioRecorderPlayerPackage} from '@react-native-oh-tpl/react-native-audio-recorder-player/ts';
 
 export function createRNPackages(ctx: RNPackageContext): RNPackage[] {
   return [
     new FastImagePackage(ctx),
     new SvgPackage(ctx),
     new SecureRandomPackage(ctx),
-    new WebViewPackage(ctx)
+    new WebViewPackage(ctx),
+    new RNAudioRecorderPlayerPackage(ctx)
 	];
 }
