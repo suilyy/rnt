f0a6568c509bb88c972944ad007b333a3b9f5e5d
wushuwang <wushuwang1@h-partners.com>
2024-10-22 16:23:28 +0800

    feat: 添加多文件夹用例

---
+++ 2024-phase3-tester/platform-colors.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "prefix": "rnpc_",
+  "colors": {
+    "background": {
+      "light": "#ffffff",
+      "dark": "#000000"
+    },
+    "text": "#696969",
+    "accent": "pink",
+    "contrasted": "#FFF0F5",
+  },
+  "javascript": {
+    "typescript": true,
+    "outputDirectory": "src/colors/"
+  },
+  "ios": {
+    "outputDirectory": "ios/app_name/Images.xcassets/"
+  },
+  "android": {
+    "outputDirectory": "android/app/src/main/res/"
+  },
+  "harmony": {
+    "outputDirectory": "harmony/AppScope/resources/"
+  }
+};
