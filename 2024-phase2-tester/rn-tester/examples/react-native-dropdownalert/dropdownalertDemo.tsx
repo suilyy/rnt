1925f1ffc9c2d44b42cefab89444bc61d346ca61
zy432099 <46698434+zy432099@users.noreply.github.com>
2024-11-11 15:13:11 +0800

    react-native-dropdownalert引入方式更改

--- 2024-phase2-tester/rn-tester/examples/react-native-dropdownalert/dropdownalertDemo.tsx
+++ 2024-phase2-tester/rn-tester/examples/react-native-dropdownalert/dropdownalertDemo.tsx
@@ -531,4 +531,19 @@ const styles = StyleSheet.create({
   },
 });
 
-export default DropdownalertDemo;
+// 或者使用 export default 导出
+export default {
+  displayName: "react-native-dropdownalert",
+  framework: "React",
+  category: "UI",
+  title: "react-native-dropdownalert-DropdownalertDemo",
+  description: "react-native-dropdownalert",
+  examples: [
+      {
+          title: "react-native-dropdownalert",
+          render: function (): any {
+              return <DropdownalertDemo />;
+          },
+      },
+  ],
+};
