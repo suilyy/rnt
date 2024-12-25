e55859357fdcd3b169519f0577274195e7a1d859
Qy742698 <837185575@qq.com>
2024-12-25 10:02:56 +0800

    test:  添加代码规避警告提示

--- 2024-phase4-tester/index.js
+++ 2024-phase4-tester/index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import RNTester from './rn-tester';
 import { name as appName } from './app.json';
+
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 
 AppRegistry.registerComponent(appName, () => RNTester);
