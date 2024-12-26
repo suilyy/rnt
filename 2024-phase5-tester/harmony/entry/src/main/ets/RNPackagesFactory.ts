3f6354fe803a639a8d617f14d6ebcee100989ea6
sunshinezjb <zhengjunbo68@126.com>
2024-12-26 12:09:56 +0800

    delete config demo

--- 2024-phase5-tester/harmony/entry/src/main/ets/RNPackagesFactory.ts
+++
@@ -1,9 +0,0 @@
-import type {RNPackageContext, RNPackage} from '@rnoh/react-native-openharmony/ts';
-import { DocViewPackage } from "@react-native-oh-tpl/react-native-doc-viewer/ts"
-
-
-export function createRNPackages(ctx: RNPackageContext): RNPackage[] {
-  return [
-    new DocViewPackage(ctx)
-  ];
-}
