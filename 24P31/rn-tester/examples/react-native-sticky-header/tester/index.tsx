3c4089fcc887bf5676d0d9686b4c6537a1b5f33f
xbh0902 <xbh0902@gmail.com>
2025-01-23 17:06:27 +0800

    test: 修改react-native-sticky-parallax-header显示名称

--- 24P31/rn-tester/examples/react-native-sticky-header/tester/index.tsx
+++ 24P31/rn-tester/examples/react-native-sticky-header/tester/index.tsx
@@ -4,13 +4,13 @@ export default App;
 export const displayName = 'ReactNativeStickyHeader';
 export const framework = 'React';
 export const category = 'UI';
-export const title = 'react-native-sticky-header';
+export const title = 'react-native-sticky-parallax-header';
 export const documentationURL = 'https://github.com/react-native-oh-library/react-native-send-intent';
 export const description = 'React Native Sticky Header';
 
 export const examples = [
   {
-    title: 'react-native-sticky-header',
+    title: 'react-native-sticky-parallax-header',
     render: function (): any {
       return <App />;
     },
