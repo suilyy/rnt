38724c2e8641f5cda21c4175da0126d7f387fe43
ding_chengjie <dingchenjie1@h-partners.com>
2025-02-26 23:57:48 +0800

    test:删除24P6中多余的svg demo

--- 24P6/harmony/entry/src/main/cpp/PackageProvider.cpp
+++ 24P6/harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,6 +1,5 @@
 #include "RNOH/PackageProvider.h"
 #include "generated/RNOHGeneratedPackage.h"
-#include "SVGPackage.h"
 #include "RealmPackage.h"
 #include "RNBuglyPackage.h"
 #include "SearchBarPackage.h"
@@ -12,7 +11,6 @@ std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(
     Package::Context ctx) {
   return {
     std::make_shared<RNOHGeneratedPackage>(ctx),
-    std::make_shared<SVGPackage>(ctx),
 	std::make_shared<RealmPackage>(ctx),
 	std::make_shared<RNBuglyPackage>(ctx),
 	std::make_shared<SearchBarPackage>(ctx),
