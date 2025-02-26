26c47aac544859e0e5866cdd525e2a7c96d77e12
ding_chengjie <dingchenjie1@h-partners.com>
2025-02-27 00:39:37 +0800

    删除search-bar组件多余的配置

--- 24P6/harmony/entry/src/main/ets/RNPackagesFactory.ts
+++ 24P6/harmony/entry/src/main/ets/RNPackagesFactory.ts
@@ -2,7 +2,6 @@ import type {RNPackageContext, RNPackage} from '@rnoh/react-native-openharmony/t
 import { SslPinningPackage } from "@react-native-oh-tpl/react-native-ssl-pinning/ts";
 import {RNRealmPackage} from '@react-native-oh-tpl/realm/ts';
 import {RNBuglyPackage} from '@react-native-oh-tpl/rn-bugly/ts';
-import {SearchBarPackage} from '@react-native-oh-tpl/react-native-search-bar/ts';
 import {RNFileSelectorPackage}  from '@react-native-oh-tpl/react-native-file-selector/ts';
 import {SafeAreaViewPackage} from '@react-native-oh-tpl/react-native-safe-area-context/ts';
 
@@ -12,7 +11,6 @@ export function createRNPackages(ctx: RNPackageContext): RNPackage[] {
     new SslPinningPackage(ctx),
 	  new RNRealmPackage(ctx),
 	  new RNBuglyPackage(ctx),
-	  new SearchBarPackage(ctx),
     new RNFileSelectorPackage(ctx),
     new SafeAreaViewPackage(ctx),
   ];
