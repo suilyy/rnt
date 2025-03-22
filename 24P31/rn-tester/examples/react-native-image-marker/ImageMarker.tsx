06fdf3ba98614df4c78a4b9066ec15f309685c61
qinjianqili <qinjianqi@h-partners.com>
2025-03-22 16:43:17 +0800

    fix: 修改react-native-image-marker组件示例Image的 resizeMode属性值

--- 24P31/rn-tester/examples/react-native-image-marker/ImageMarker.tsx
+++ 24P31/rn-tester/examples/react-native-image-marker/ImageMarker.tsx
@@ -2309,7 +2309,7 @@ export const ImageMarker = () => {
                 <Text style={styles.sectionTitle}>
                   {url_backscale0_5}
                 </Text>
-                <Image resizeMode='center' source={{ uri: url_backscale0_5, width: 300, height: 150 }} />
+                <Image resizeMode='contain' source={{ uri: url_backscale0_5, width: 300, height: 150 }} />
               </View>
             </View>
           </TestCase>
@@ -2329,7 +2329,7 @@ export const ImageMarker = () => {
                 <Text style={styles.sectionTitle}>
                   {url_backscale1_5}
                 </Text>
-                <Image resizeMode='center' source={{ uri: url_backscale1_5, width: 300, height: 150 }} />
+                <Image resizeMode='contain' source={{ uri: url_backscale1_5, width: 300, height: 150 }} />
               </View>
             </View>
           </TestCase>
@@ -2349,7 +2349,7 @@ export const ImageMarker = () => {
                 <Text style={styles.sectionTitle}>
                   {url_scale0_5}
                 </Text>
-                <Image resizeMode='center' source={{ uri: url_scale0_5, width: 300, height: 150 }} />
+                <Image resizeMode='contain' source={{ uri: url_scale0_5, width: 300, height: 150 }} />
               </View>
             </View>
           </TestCase>
@@ -2369,7 +2369,7 @@ export const ImageMarker = () => {
                 <Text style={styles.sectionTitle}>
                   {url_scale1_5}
                 </Text>
-                <Image resizeMode='center' source={{ uri: url_scale1_5, width: 300, height: 150 }} />
+                <Image resizeMode='contain' source={{ uri: url_scale1_5, width: 300, height: 150 }} />
               </View>
             </View>
           </TestCase>
